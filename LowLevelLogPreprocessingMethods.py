aedbd5d365a09114ef2965f9154694b8673b5ff7
antoninabegiceva <be-ton@yandex.ru>
2022-05-24 03:19:11 +0300

    fix log preprocessing process for acyclic traces

--- LowLevelLogPreprocessingMethods.py
+++ LowLevelLogPreprocessingMethods.py
@@ -12,26 +12,27 @@ def hierarchical_events_mapping(json_file_name):
 def detailed_events_to_abstract(trace, mapping):
     abstract_trace = []
     for detailed_event in trace:
-        keys = [key for key, value in mapping.items() if detailed_event in value]
+        keys = [key for key in mapping.keys() if detailed_event in mapping[key]]
+        #keys = [key for key, value in mapping if detailed_event in value]
         abstract_event = keys[0]
         abstract_trace.append(abstract_event)  # create abstract trace with duplicates
     i = 0
     abstract_trace = remove_stuttering(abstract_trace)
-    abstract_traces = []
-    abstract_traces.append(abstract_trace)
+    abstract_traces = [abstract_trace]
     for event in set(abstract_trace):
-        for trace in abstract_traces:
-            traces_with_event = generate_traces_by_duplicated_events(abstract_trace, event)
-            for draft_trace in traces_with_event:
-                draft_trace = remove_stuttering(draft_trace)
-            abstract_traces.remove(trace)
-            abstract_traces.extend(traces_with_event)
+        if abstract_trace.count(event) > 1:
+            for trace in abstract_traces:
+                traces_with_event = generate_traces_by_duplicated_events(trace, event)
+                for draft_trace in traces_with_event:
+                    draft_trace = remove_stuttering(draft_trace)
+                abstract_traces.remove(trace)
+                abstract_traces.extend(traces_with_event)
     return abstract_traces
 
 
 def remove_stuttering(trace):
     i = 0
-    while i < len(trace) - 2:
+    while i < len(trace) - 1:
         if trace[i] == trace[i + 1]:
             del trace[i + 1]  # remove stuttering
         else:
@@ -42,15 +43,24 @@ def remove_stuttering(trace):
 def generate_traces_by_duplicated_events(trace, event):
     traces = []
     indexes = []
+    k = 0
     for i in range(len(trace)):
         if trace[i] == event:
-            indexes.append(i)
+            indexes.append(k)
+            k += 1
     for index in indexes:
         changed_trace = trace
         new_indexes = indexes
+        j = 0
+        i = 0
+        while i < len(changed_trace):
+            if changed_trace[i] == event:
+                if j != index:
+                    del changed_trace[i]
+                    i -= 1
+                j += 1
+            i += 1
         new_indexes.remove(index)
-        for i in sorted(new_indexes, reverse=True):
-            del changed_trace[i]
         traces.append(changed_trace)
     if len(traces) == 0:
         traces.append(trace)
--- main.py
+++ main.py
@@ -1,21 +1,326 @@
 import os
 import pm4py
+from copy import deepcopy
+from pm4py.objects.log.obj import EventLog, Trace, Event
 
 from TInvRecogniser import TInvRecogniser
 import LowLevelLogPreprocessingMethods as preprocessing
 
 
 def main():
-    file_path = os.path.join(os.path.dirname(__file__), 'from_paper.xes')
+    file_path = os.path.join(os.path.dirname(__file__), 'сеть поболбше.1.2251028129059564290.xes')
     log = pm4py.read.read_xes(file_path)
-   # t_inv = TInvRecogniser(log)
-   # t_inv.fill_t_inv()
-   # print(t_inv.t_invariants)
+    t_inv = TInvRecogniser(log)
+    t_inv.fill_t_inv()
+    print(t_inv.t_invariants)
 
     #mapping
     file_path = os.path.join(os.path.dirname(__file__), 'mapping.json')
     mapping = preprocessing.hierarchical_events_mapping(file_path)
 
+    abstract_traces = []
+    for trace in log:
+        trace_to_events_names_list = [event['concept:name'] for event in trace]
+        abstract_traces.extend(preprocessing.detailed_events_to_abstract(trace_to_events_names_list, mapping))
+    print(abstract_traces)
+
+def generate_large_log():
+    L = EventLog()
+    t0 = Event()
+    t0["concept:name"] = "t0"
+    t1 = Event()
+    t1["concept:name"] = "t1"
+    t2 = Event()
+    t2["concept:name"] = "t2"
+    t3 = Event()
+    t3["concept:name"] = "t3"
+    t4 = Event()
+    t4["concept:name"] = "t4"
+    t5 = Event()
+    t5["concept:name"] = "t5"
+    t6 = Event()
+    t6["concept:name"] = "t6"
+    t7 = Event()
+    t7["concept:name"] = "t7"
+    t8 = Event()
+    t8["concept:name"] = "t8"
+    t9 = Event()
+    t9["concept:name"] = "t9"
+    t10 = Event()
+    t10["concept:name"] = "t10"
+    t11 = Event()
+    t11["concept:name"] = "t11"
+    t12 = Event()
+    t12["concept:name"] = "t12"
+    t13 = Event()
+    t13["concept:name"] = "t13"
+    t14 = Event()
+    t14["concept:name"] = "t14"
+    t15 = Event()
+    t15["concept:name"] = "t15"
+    t16 = Event()
+    t16["concept:name"] = "t16"
+    t17 = Event()
+    t17["concept:name"] = "t17"
+    t18 = Event()
+    t18["concept:name"] = "t18"
+    t19 = Event()
+    t19["concept:name"] = "t19"
+    t20 = Event()
+    t20["concept:name"] = "t20"
+    t21 = Event()
+    t21["concept:name"] = "t21"
+    t22 = Event()
+    t22["concept:name"] = "t22"
+    t23 = Event()
+    t23["concept:name"] = "t23"
+    t24 = Event()
+    t24["concept:name"] = "t24"
+    t25 = Event()
+    t25["concept:name"] = "t25"
+    t26 = Event()
+    t26["concept:name"] = "t26"
+    t27 = Event()
+    t27["concept:name"] = "t27"
+    t = Trace() #без циклов
+    t.append(t0)
+    t.append(t1)
+    t.append(t3)
+    t.append(t13)
+    t.append(t4)
+    t.append(t5)
+    t.append(t6)
+    t.append(t14)
+    t.append(t15)
+    t.append(t7)
+    t.append(t8)
+    t.append(t9)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t25)
+    t.append(t26)
+    t.append(t27)
+    for i in range(3):
+        L.append(deepcopy(t))
+    t = Trace() #без циклов
+    t.append(t0)
+    t.append(t2)
+    t.append(t3)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t25)
+    t.append(t26)
+    t.append(t27)
+    for i in range(4):
+        L.append(deepcopy(t))
+    t = Trace()  # без циклов
+    t.append(t0)
+    t.append(t2)
+    t.append(t3)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t22)
+    t.append(t23)
+    t.append(t24)
+    for i in range(4):
+        L.append(deepcopy(t))
+    t = Trace() #c циклом
+    t.append(t0)
+    t.append(t2)
+    t.append(t3)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t19)
+    t.append(t20)
+    t.append(t21)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t25)
+    t.append(t26)
+    t.append(t27)
+    for i in range(4):
+        L.append(deepcopy(t))
+    t = Trace()  # без циклов
+    t.append(t0)
+    t.append(t2)
+    t.append(t3)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t19)
+    t.append(t20)
+    t.append(t21)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t19)
+    t.append(t20)
+    t.append(t21)
+    t.append(t13)
+    t.append(t4)
+    t.append(t5)
+    t.append(t6)
+    t.append(t14)
+    t.append(t15)
+    t.append(t7)
+    t.append(t8)
+    t.append(t9)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t22)
+    t.append(t23)
+    t.append(t24)
+    for i in range(4):
+        L.append(deepcopy(t))
+    return L
+
+def generate_small_log():
+    L = EventLog()
+    t0 = Event()
+    t0["concept:name"] = "t0"
+    t1 = Event()
+    t1["concept:name"] = "t1"
+    t2 = Event()
+    t2["concept:name"] = "t2"
+    t3 = Event()
+    t3["concept:name"] = "t3"
+    t4 = Event()
+    t4["concept:name"] = "t4"
+    t5 = Event()
+    t5["concept:name"] = "t5"
+    t6 = Event()
+    t6["concept:name"] = "t6"
+    t7 = Event()
+    t7["concept:name"] = "t7"
+    t8 = Event()
+    t8["concept:name"] = "t8"
+    t9 = Event()
+    t9["concept:name"] = "t9"
+    t10 = Event()
+    t10["concept:name"] = "t10"
+    t11 = Event()
+    t11["concept:name"] = "t11"
+    t12 = Event()
+    t12["concept:name"] = "t12"
+    t13 = Event()
+    t13["concept:name"] = "t13"
+    t14 = Event()
+    t14["concept:name"] = "t14"
+    t15 = Event()
+    t15["concept:name"] = "t15"
+    t16 = Event()
+    t16["concept:name"] = "t16"
+    t17 = Event()
+    t17["concept:name"] = "t17"
+    t18 = Event()
+    t18["concept:name"] = "t18"
+    t19 = Event()
+    t19["concept:name"] = "t19"
+    t20 = Event()
+    t20["concept:name"] = "t20"
+    t21 = Event()
+    t21["concept:name"] = "t21"
+    t22 = Event()
+    t22["concept:name"] = "t22"
+    t23 = Event()
+    t23["concept:name"] = "t23"
+    t24 = Event()
+    t24["concept:name"] = "t24"
+    t25 = Event()
+    t25["concept:name"] = "t25"
+    t26 = Event()
+    t26["concept:name"] = "t26"
+    t27 = Event()
+    t27["concept:name"] = "t27"
+    t = Trace() #без циклов
+    t.append(t0)
+    t.append(t2)
+    t.append(t3)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t25)
+    t.append(t26)
+    t.append(t27)
+    L.append(deepcopy(t))
+    t = Trace() #c циклом
+    t.append(t0)
+    t.append(t2)
+    t.append(t3)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t19)
+    t.append(t20)
+    t.append(t21)
+    t.append(t13)
+    t.append(t10)
+    t.append(t11)
+    t.append(t14)
+    t.append(t15)
+    t.append(t12)
+    t.append(t16)
+    t.append(t17)
+    t.append(t18)
+    t.append(t25)
+    t.append(t26)
+    t.append(t27)
+    L.append(deepcopy(t))
+    return L
 
 if __name__ == '__main__':
     main()
